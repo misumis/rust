5d70a2a96872ecc92c34e355af85d2920015c669
Michał Nowak <michal@enuai.pl>
2022-07-01 21:09:40 +0200

    guessing game

---
+++ chapter2/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand;
+
+fn main() {
+    let generated_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        let mut guess = String::new();
+        println!("Guess the number: ");
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        if guess < generated_number {
+            println!("Too low!");
+        } else if guess > generated_number {
+            println!("Too high!");
+        } else {
+            println!("You win!");
+            break;
+        }
+    } 
+}
